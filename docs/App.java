8de968d72bf5ee659b8c14555dd4252eebf89be4
Laania11 <alopezl90@miumg.edu.gt>
2026-02-14 10:04:06 -0600

    tarea ejercicios

---
+++ docs/App.java
@@ -0,0 +1,28 @@
+package com.tarea_uno.App.Java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola mundo desde java!" );
+        System.out.println();
+        System.out.println();
+        
+        System.out.println( "Multiplicacion " );
+        int num1 = 5;
+        int num2 = 9;
+        
+        int resultadoMultiplicacion = num1 * num2;
+        
+        System.out.println("El primer numero es:" + num1); 
+        System.out.println("El segundo numero es:" + num2);
+        System.out.println("El resultado es:" + resultadoMultiplicacion);
+       
+        
+        
+    }
+}
